b35a0b178feacfbaf62ca15b87c4573d7388be27
Nirjhar <nirjharmaheshwari156@gmail.com>
2020-09-02 16:56:54 +0530

    Updates

---
+++ precache-manifest.b4ecef5f9acdc7c4ae1770a360175b75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ff63f659679800febec34bb0644000",
+    "url": "/burgerBuilder/index.html"
+  },
+  {
+    "revision": "31d898e43d5023464336",
+    "url": "/burgerBuilder/static/css/main.29ad5fef.chunk.css"
+  },
+  {
+    "revision": "4951223293de94f5f2f9",
+    "url": "/burgerBuilder/static/js/2.200da531.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burgerBuilder/static/js/2.200da531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d898e43d5023464336",
+    "url": "/burgerBuilder/static/js/main.2784f04c.chunk.js"
+  },
+  {
+    "revision": "ccee035f84b69ae3575e",
+    "url": "/burgerBuilder/static/js/runtime-main.ed5faca4.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
